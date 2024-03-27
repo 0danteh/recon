ea5f484345699564d19bd1b0fce921deb2d6d7c4
Dante <francescopolymathes@gmail.com>
2024-03-27 18:28:27 +0100

    Update

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -4,3 +4,4 @@ def remove_duplicates():
     print("Note: Enter the full path to the folder and the file name including the .txt extension in the format 'path/to/folder/[example].txt'.")
     file_path = input("Enter the full path to the folder and the file name: ")
     if os.path.exists(file_path):
+        try:
