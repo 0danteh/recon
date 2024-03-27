f49f71c55eead45995e103f29d7703479d63df74
Dante <francescopolymathes@gmail.com>
2024-03-27 18:27:17 +0100

    Update

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,3 +1,5 @@
 import os
 
 def remove_duplicates():
+    print("Note: Enter the full path to the folder and the file name including the .txt extension in the format 'path/to/folder/[example].txt'.")
+    
