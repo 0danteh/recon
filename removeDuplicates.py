72037d916d627633b1d55d8ea7dedd0a6b1bc879
Dante <francescopolymathes@gmail.com>
2024-03-27 17:59:15 +0100

    Update

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -2,4 +2,4 @@ def remove_duplicates():
     print("Note: Remember to keep the .txt extension when entering the input file name.")
     file_name = input("Enter the name of the .txt file: ")
     try:
-        with open(file_name as 'r') as file:
+        with open(file_name, 'r') as file:
