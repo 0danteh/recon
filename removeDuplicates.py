ea8196e8d11205041a9c041e9c7cb9eebf33f222
Dante <francescopolymathes@gmail.com>
2024-03-27 18:40:34 +0100

    Update

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -4,3 +4,10 @@ def remove_duplicates(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
 
+    unique_lines = list(set(lines))
+    duplicates_count = len(lines) - len(unique_lines)
+    with open(file_name, 'w') as file:
+        file.writelines(unique_lines)
+    print(f"{duplicates_count} duplicates were deleted.")
+    print(f"Number of items at the beginning: {len(lines)}, Number of items at the end: {len(unique_lines)}")
+
