cc14236c0085fcb68cddd84312174335190ee01c
Dante <francescopolymathes@gmail.com>
2024-03-27 18:50:56 +0100

    Update

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,5 +1,18 @@
 import argparse
 
+name = """
+______                               ______             _ _           _            
+| ___ \                              |  _  \           | (_)         | |           
+| |_/ /___ _ __ ___   _____   _____  | | | |_   _ _ __ | |_  ___ __ _| |_ ___  ___ 
+|    // _ \ '_ ` _ \ / _ \ \ / / _ \ | | | | | | | '_ \| | |/ __/ _` | __/ _ \/ __|
+| |\ \  __/ | | | | | (_) \ V /  __/ | |/ /| |_| | |_) | | | (_| (_| | ||  __/\__ \
+\_| \_\___|_| |_| |_|\___/ \_/ \___| |___/  \__,_| .__/|_|_|\___\__,_|\__\___||___/
+                                                | |                               
+                                                |_|                               
+"""
+
+print(name)
+
 def remove_duplicates(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
@@ -18,20 +31,7 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--about', action='store_true', help='Display help message')
     args = parser.parse_args()
 
-    name = """
-    ______                               ______             _ _           _            
-    | ___ \                              |  _  \           | (_)         | |           
-    | |_/ /___ _ __ ___   _____   _____  | | | |_   _ _ __ | |_  ___ __ _| |_ ___  ___ 
-    |    // _ \ '_ ` _ \ / _ \ \ / / _ \ | | | | | | | '_ \| | |/ __/ _` | __/ _ \/ __|
-    | |\ \  __/ | | | | | (_) \ V /  __/ | |/ /| |_| | |_) | | | (_| (_| | ||  __/\__ \
-    \_| \_\___|_| |_| |_|\___/ \_/ \___| |___/  \__,_| .__/|_|_|\___\__,_|\__\___||___/
-                                                    | |                               
-                                                    |_|                               
-
-    """
-
     if args.about:
-        print(name)
         print("This script removes duplicates from a text file.")
         print("Flags:")
         print("-n: Stands for 'name' (name of the file)")
