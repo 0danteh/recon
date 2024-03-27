f9d052b457852670db09ba660274c173b17e076a
Dante <francescopolymathes@gmail.com>
2024-03-27 18:29:02 +0100

    Update

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -11,3 +11,5 @@ def remove_duplicates():
             initial_count = len(lines)
             unique_lines = list(set(lines))
 
+            with open(file_path, 'w') as file:
+                file.writelines(unique_lines)
